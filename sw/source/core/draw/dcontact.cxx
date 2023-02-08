d47feb759203542fb191a113bc53ee5cb88fea95
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2023-02-08 10:42:19 +0900

    use Range2DLWrap for "OutRectangle" in SdrObject
    Change-Id: I243b44a84bc09991744009ae24ac7657d493c5cf

--- sw/source/core/draw/dcontact.cxx
+++ sw/source/core/draw/dcontact.cxx
@@ -2428,24 +2428,22 @@ void SwDrawVirtObj::NbcSetAnchorPos(const Point& rPnt)
 
 const tools::Rectangle& SwDrawVirtObj::GetCurrentBoundRect() const
 {
-    if (m_aOutRect.IsEmpty())
-    {
+    if (isOutRectangleEmpty())
         const_cast<SwDrawVirtObj*>(this)->RecalcBoundRect();
-    }
 
-    return m_aOutRect;
+    return getOutRectangle();
 }
 
 const tools::Rectangle& SwDrawVirtObj::GetLastBoundRect() const
 {
-    return m_aOutRect;
+    return getOutRectangle();
 }
 
 Point SwDrawVirtObj::GetOffset() const
 {
     // do NOT use IsEmpty() here, there is already a useful offset
     // in the position
-    if (m_aOutRect == tools::Rectangle())
+    if (getOutRectangle() == tools::Rectangle())
     {
         return Point();
     }
